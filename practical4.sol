b779044d3c18176f75ab4e4ed4b10b728fc9dda4
Atharva Kumkar <kumkaratharva8@gmail.com>
2026-08-04 13:32:37 +0530

    Update practical4.sol

--- practical4.sol
+++ practical4.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Donation {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Accept donations
+    function donate() public payable {
+    }
+
+    // View contract balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    // Transfer entire balance to owner
+    function transferToOwner() public {
+        require(msg.sender == owner, "Only owner can withdraw");
+
+        payable(owner).transfer(address(this).balance);
+    }
+}
+
