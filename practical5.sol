03aa50a77277d5ea11a881395f867ec6985e3245
Atharva Kumkar <kumkaratharva8@gmail.com>
2026-08-16 18:22:04 +0530

    Refactor withdraw function for safer Ether transfer
    Updated the withdraw function to use call for safer transfers.

--- practical5.sol
+++ practical5.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -6,7 +5,6 @@ contract Donation {
 
     address public owner;
 
-    // Custom Errors
     error InsufficientBalance(uint availableBalance);
     error MinimumDonation(uint sentAmount);
 
@@ -14,30 +12,25 @@ contract Donation {
         owner = msg.sender;
     }
 
-    // Accept Donation
     function donate() public payable {
 
-        // Donor must have at least 100000 Wei after sending transaction
         if (msg.sender.balance < 100000) {
             revert InsufficientBalance(msg.sender.balance);
         }
 
-        // Minimum donation should be 5 Gwei
         if (msg.value < 5 gwei) {
             revert MinimumDonation(msg.value);
         }
     }
 
-    // View Contract Balance
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
-    // Withdraw Contract Balance
     function withdraw() public {
-
         require(msg.sender == owner, "Only owner can withdraw");
 
-        payable(owner).transfer(address(this).balance);
+        (bool success, ) = payable(owner).call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
 }
