8a775f58936cd8536b99cc1f418ccd1561a720d5
Atharva Kumkar <kumkaratharva8@gmail.com>
2026-09-05 19:30:12 +0530

    Implement Calculator and MainContract for arithmetic

--- practical9.sol
+++ practical9.sol
@@ -1,1 +1,55 @@
+// Calculator.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Calculator {
+
+    // Function to add two numbers
+    function add(uint a, uint b) public pure returns (uint) {
+        return a + b;
+    }
+
+    // Function to subtract two numbers
+    function subtract(uint a, uint b) public pure returns (uint) {
+        return a - b;
+    }
+}
+
+
+
+//MainContract.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./Calculator.sol";
+
+contract MainContract {
+
+    Calculator public calculator;
+
+    constructor() {
+        calculator = new Calculator();
+    }
+
+    // Use add function from Calculator contract
+    function calculateSum(uint a, uint b)
+        public
+        view
+        returns (uint)
+    {
+        return calculator.add(a, b);
+    }
+
+    // Use subtract function from Calculator contract
+    function calculateDifference(uint a, uint b)
+        public
+        view
+        returns (uint)
+    {
+        return calculator.subtract(a, b);
+    }
+}
+
 
