0b9fdc8b279936b38156de91e4b1ace8b68de9bb
Atharva Kumkar <kumkaratharva8@gmail.com>
2026-09-05 19:27:04 +0530

    Implement Donation contract with owner and donation features

--- practical6.sol
+++ practical6.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Donation {
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Modifier to allow only owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    // Accept donations
+    function donate() public payable {
+    }
+
+    // View contract balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    // Withdraw contract balance
+    function withdraw() public onlyOwner {
+        (bool success, ) = payable(owner).call{
+            value: address(this).balance
+        }("");
+
+        require(success, "Transfer failed");
+    }
+}
